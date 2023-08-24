3893bf6239ed02053052ed8e125514349a02a1c6
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-08-24 23:25:33 +0530

    Header CartIcon

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,7 +1,8 @@
  import React,{Fragment} from 'react'
 
  import classes from '../Header.module.css';
-
+ import HeaderCartButton from './HeaderCartButton';
+ 
  import mealsimage from '../../assets/meals.jpg';
  
  const Header = () => {
@@ -9,9 +10,8 @@
      <Fragment>
         <header  className={classes.header}>
             <h1>React Meals</h1>
-            <button>
-                Cart
-            </button>
+            <HeaderCartButton/>
+            
         </header>
         <div className={classes['main-image']}>
             <img src={mealsimage} alt="This is a food table" />
