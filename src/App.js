2851f91e446881b177180f0f9907fdb976d7ea43
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-08-24 23:43:35 +0530

    Add Restaurant summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{Fragment} from "react";
 
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
    <Fragment>
     <Header/>
+    <Meals/>
    </Fragment>
   );
 }
