ba03da05b99344f69afed8090ae5f98a53150752
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-08-28 18:10:12 +0530

    Cart open & close functionality

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,14 +10,14 @@ const Cart = props => {
     );
     
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {carItems}
             <div className={classes.total}>
             <span>Total Amount</span>
             <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['botton--alt']}>Close</button>
+                <button className={classes['botton--alt']} onClick={props.onClose}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal >
